af55a65701e5ee3d3a96a14b8a246daaae6d504b
arthur <arthurfsilva110@gamial.com>
2023-10-23 15:12:36 -0300

    modifying nginx locations.conf file

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -1,19 +1,22 @@
 package Application;
 import java.io.*;
+import java.nio.file.Files;
+import java.security.spec.ECField;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static final GeneralData GENERAL_DATA = new GeneralData();
     public static void main(String[] args) throws IOException, InterruptedException {
-        do{
-            startNewMonitoramentoStatement();
-        }while(wasNotStarted());
-
+            startNewMonitoramentoStatement(args[0]);
+        //GENERAL_DATA.dockerExitPort = 5043;
+        //addContainerToServer("back");
+        //    removeContainerFromServer(String.valueOf(5042));
     }
 
-    public static void startNewMonitoramentoStatement() throws IOException, InterruptedException {
+    public static void startNewMonitoramentoStatement(String containerType) throws IOException, InterruptedException {
         GENERAL_DATA.dockerExitPort = getNextPort();
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        String dockerRunString = "docker run -d -p " + GENERAL_DATA.dockerExitPort + ":12428 --name sae_monitoramento_back-" + GENERAL_DATA.dockerExitPort + " -it " + "sae_monitoramento_back";
+        String dockerRunString = "docker run -d -p " + GENERAL_DATA.dockerExitPort + ":12428 --name sae_monitoramento_" + containerType + "-" + GENERAL_DATA.dockerExitPort + " -it " + "sae_monitoramento_" + containerType;
         String command = "echo " + dockerRunString + " > " + GENERAL_DATA.pipefile;
 
         Process process = Runtime.getRuntime().exec(dockerRunString);
@@ -29,7 +32,7 @@ public class Application {
                 write.write(stdoutLine);
                 write.newLine();
 
-                writeContainerUp.write(stdoutLine);
+                writeContainerUp.write(GENERAL_DATA.dockerExitPort+";"+stdoutLine);
                 writeContainerUp.newLine();
             }
 
@@ -101,5 +104,62 @@ public class Application {
         File file = new File("output.txt");
         return file.length() == 0;
     }
+
+    public static void addContainerToServer(String containerType){
+        try(BufferedWriter locationsConf = new BufferedWriter(new FileWriter(GENERAL_DATA.locationsConf, true))){
+            locationsConf.write(getNewNginxContainerString(containerType));
+
+        }catch(Exception f){
+            System.out.println("Error writing portFile: " + f.getMessage());
+        }
+    }
+
+    public static void removeContainerFromServer(String containerPort){
+        File locationsDotConf = new File(GENERAL_DATA.locationsConf);
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(locationsDotConf))) {
+            File temporaryFile = new File(GENERAL_DATA.locationsConfDir + "tempLocations.conf");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(temporaryFile, false));
+            String line;
+
+            while((line = reader.readLine()) != null){
+                if(!line.contains("#"+containerPort)){
+                    writer.write(line + "\n");
+                }
+            }
+
+            writer.close();
+
+            if(Files.size(temporaryFile.toPath()) != 0){
+                locationsDotConf.delete();
+            }
+
+
+            File rename = new File(GENERAL_DATA.locationsConf);
+            temporaryFile.renameTo(rename);
+
+        }catch (Exception e){
+
+        }
+    }
+
+    public static String getNewNginxContainerString(String containerType){
+        String identificationTAG = "\t#" + GENERAL_DATA.dockerExitPort + "\n";
+        String container = "sae_monitoramento_" + containerType + "-";
+        String monitoramentoHeader = "#" + container + GENERAL_DATA.dockerExitPort + identificationTAG;
+        return          identificationTAG +
+                        monitoramentoHeader +
+                        "   location /selatiot/" + container + GENERAL_DATA.dockerExitPort + "{" + identificationTAG +
+                        "      proxy_pass http://" + container + GENERAL_DATA.dockerExitPort + ":" + GENERAL_DATA.dockerExitPort + ";" + identificationTAG +
+                        "      rewrite ^/" + container + GENERAL_DATA.dockerExitPort + "(.*)$ $1 break;" + identificationTAG +
+                        "   }" + identificationTAG +
+                        identificationTAG +
+                        "   location /" + container + GENERAL_DATA.dockerExitPort + "{" + identificationTAG  +
+                        "      proxy_pass http://" + container + GENERAL_DATA.dockerExitPort + ":" + GENERAL_DATA.dockerExitPort + ";" + identificationTAG  +
+                        "      rewrite ^/" + container + GENERAL_DATA.dockerExitPort + "(.*)$ $1 break;"  + identificationTAG  +
+                        "   }" + identificationTAG +
+                        identificationTAG;
+    }
+
 
 }
--- src/main/java/Application/GeneralData.java
+++ src/main/java/Application/GeneralData.java
@@ -3,6 +3,74 @@ public class GeneralData {
     //public String logOutputPath = "/home/arthur/lri/output.log";
     //public String portFile = "/home/arthur/lri/port.txt";
     public String portFile = "/usr/local/execpipe/port.txt";
-    public String pipefile = "pipefile";
+    //public String locationsConfDir = "/root/Documents/configuracoesservidores/server_dev/nginx/";
+    public String locationsConfDir = "/home/arthur/";
+    public String locationsConf = locationsConfDir + "locations.conf";
     public int dockerExitPort;
+    public String pipefile = "pipefile";
+
+
+    public String originalLocations = "location /selatiot/sae_cadastro_front {\n" +
+            "      proxy_pass http://sae_cadastro_front:12425;\n" +
+            "      rewrite ^/sae_cadastro_front(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /sae_cadastro_front/ {\n" +
+            "      proxy_pass http://sae_cadastro_front:12425;\n" +
+            "      rewrite ^/sae_cadastro_front(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "    location /selatiot/sae_monitoramento_front {\n" +
+            "      proxy_pass http://sae_monitoramento_front:12430;\n" +
+            "      rewrite ^/sae_monitoramento_front(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /sae_monitoramento_front/ {\n" +
+            "      proxy_pass http://sae_monitoramento_front:12430;\n" +
+            "      rewrite ^/sae_monitoramento_front(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /selatiot/twinsfront {\n" +
+            "      proxy_pass http://twinsfront:4200;\n" +
+            "      rewrite ^/twinsfront(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "    location /twinsfront/ {\n" +
+            "      proxy_pass http://twinsfront:4200;\n" +
+            "      rewrite ^/twinsfront(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /selatiot/dpiotfront {\n" +
+            "      proxy_pass http://dpiotfront:4000;\n" +
+            "      rewrite ^/selatiot/dpiotfront(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "    location /dpiotfront/ {\n" +
+            "      proxy_pass http://dpiotfront:4000;\n" +
+            "      rewrite ^/dpiotfront(.*)$ $1 break;\n" +
+            "   }\n" +
+            "location /express/ {\n" +
+            "      proxy_set_header X-Script-Name /pgadmin4;\n" +
+            "      proxy_set_header X-Scheme $scheme;\n" +
+            "      proxy_set_header Host $host;\n" +
+            "      proxy_pass http://mongo-express:8081;\n" +
+            "      proxy_redirect off;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /jenkins {\n" +
+            "      proxy_pass http://10.3.192.50:8080;\n" +
+            "      proxy_set_header Host $host;\n" +
+            "      proxy_set_header X-Real-IP $remote_addr;\n" +
+            "   }\n" +
+            "\n" +
+            "   location /selatiot/auth {\n" +
+            "      proxy_pass http://selatiot_portal_backend:4001;\n" +
+            "      rewrite ^/selatiot/auth(.*)$ $1 break;\n" +
+            "   }\n" +
+            "\n" +
+            "   location / {\n" +
+            "        proxy_pass http://selatiot_portal_front:4444;\n" +
+            "   }\n";
+
+    public String newNginxLocations = "";
 }
